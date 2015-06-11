ac17bfca6d5a28c1f16f3edb41eb5b40974ae781
Fadhlan Ridhwanallah <fridhwanallah@gmail.com>
2015-06-11 11:36:19 +0700

    Membuat class anggota dan kehadiran

---
+++ WinForms/Anggota.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinForms
+{
+    class Anggota
+    {
+        private string _nip;
+        private string _nama;
+
+        public string Nip
+        {
+            get { return _nip; }
+            set { _nip = value; }
+        }
+
+        public string Nama
+        {
+            get { return _nama; }
+            set { _nama = value; }
+        }
+    }
+}
---
+++ WinForms/Kehadiran.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinForms
+{
+    class Kehadiran
+    {
+        private int _id;
+        private string _nama;
+        private DateTime _jammulai;
+        private DateTime _jamselesai;
+
+        public int ID
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string Nama
+        {
+            get { return _nama; }
+            set { _nama = value; }
+        }
+
+        public DateTime JamMulai
+        {
+            get { return _jammulai; }
+            set { _jammulai = value; }
+        }
+
+        public DateTime JamSelesai
+        {
+            get { return _jamselesai; }
+            set { _jamselesai = value; }
+        }
+    }
+}
