08fedb223b66f683e95fd11561175f15ec8f72c2
imamfzn <imamfzn@gmail.com>
2015-06-11 13:59:25 +0700

    add classs kehadiran

---
+++ WinForms/Kehadiran.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinForms
+{
+    class Kehadiran
+    {        
+        public DateTime JamDatang { get; set; }
+        public DateTime JamPulang { get; set; }
+        public Anggota Anggota { get; set; }
+        public JenisKehadiran Status { get; set; }
+        public Kegiatan Kegiatan { get; set; }
+
+        public Kehadiran(Kegiatan kegiatan, Anggota anggota, DateTime jamDatang, DateTime jamPulang, JenisKehadiran status)
+        {
+            Kegiatan = kegiatan;
+            Anggota = anggota;
+            JamDatang = jamDatang;
+            JamPulang = jamPulang;
+            Status = status;
+        }
+        public Kehadiran() { }
+
+
+    }
+}
