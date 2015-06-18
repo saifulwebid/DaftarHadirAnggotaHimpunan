1da3fc4d5684453e2af7d93d1b5b90cdf17334ee
Muhammad Saiful Islam <muhammad@saiful.web.id>
2015-06-18 11:15:34 +0700

    Memperbaiki struktur class Kegiatan

--- WinForms/Class/Kegiatan.cs
+++ WinForms/Class/Kegiatan.cs
@@ -1,52 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace WinForms
 {
     class Kegiatan
     {
-        private int _id;
-        private string _nama;
-        private DateTime _jammulai;
-        private DateTime _jamselesai;
+        public int ID { get; set; }
+        public string Nama { get; set; }
+        public DateTime JamMulai { get; set; }
+        public DateTime JamSelesai { get; set; }
+        public BindingList<Kehadiran> Kehadiran { get; private set; }
 
+        /** Constructor **/
         public Kegiatan()
         {
-
+            Kehadiran = new BindingList<Kehadiran>();
         }
-
-        public Kegiatan(int id, string nama)
-        {
-            ID = id;
-            Nama = nama;
-        }
-        public int ID
-        {
-            get { return _id; }
-            set { _id = value; }
-        }
-
-        public string Nama
-        {
-            get { return _nama; }
-            set { _nama = value; }
-        }
-
-        public DateTime JamMulai
-        {
-            get { return _jammulai; }
-            set { _jammulai = value; }
-        }
-
-        public DateTime JamSelesai
-        {
-            get { return _jamselesai; }
-            set { _jamselesai = value; }
-        }
-
-        
     }
 }
