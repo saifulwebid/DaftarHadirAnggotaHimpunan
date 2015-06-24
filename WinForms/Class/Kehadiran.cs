e55ba9555d51a72b4a1d59324837945a92fe84c9
Muhammad Saiful Islam <muhammad@saiful.web.id>
2015-06-24 11:56:58 +0700

    Menyesuaikan class Kehadiran dengan database

--- WinForms/Class/Kehadiran.cs
+++ WinForms/Class/Kehadiran.cs
@@ -3,23 +3,11 @@
 namespace WinForms.Class
 {
     class Kehadiran
-    {        
-        public DateTime JamDatang { get; set; }
-        public DateTime JamPulang { get; set; }
+    {
+        public Kegiatan Kegiatan { get; set; }
         public Anggota Anggota { get; set; }
         public JenisKehadiran Status { get; set; }
-        public Kegiatan Kegiatan { get; set; }
-
-        public Kehadiran(Kegiatan kegiatan, Anggota anggota, DateTime jamDatang, DateTime jamPulang, JenisKehadiran status)
-        {
-            Kegiatan = kegiatan;
-            Anggota = anggota;
-            JamDatang = jamDatang;
-            JamPulang = jamPulang;
-            Status = status;
-        }
-        public Kehadiran() { }
-
-
+        public DateTime JamDatang { get; set; }
+        public DateTime JamPulang { get; set; }
     }
 }
