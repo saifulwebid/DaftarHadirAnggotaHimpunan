d5e00d11c682b0af2b57e8afc6103410cb598adf
Muhammad Saiful Islam <muhammad@saiful.web.id>
2015-06-25 09:59:31 +0700

    Buat frmDetilKegiatan

---
+++ WinForms/Forms/frmDetilKegiatan.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace WinForms.Forms
+{
+    partial class frmDetilKegiatan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNama = new System.Windows.Forms.TextBox();
+            this.dtpMulai = new System.Windows.Forms.DateTimePicker();
+            this.dtpSelesai = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSimpan = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNama
+            // 
+            this.txtNama.Location = new System.Drawing.Point(12, 25);
+            this.txtNama.Name = "txtNama";
+            this.txtNama.Size = new System.Drawing.Size(200, 20);
+            this.txtNama.TabIndex = 0;
+            // 
+            // dtpMulai
+            // 
+            this.dtpMulai.CustomFormat = "dd/MM/yyyy hh:mm:ss";
+            this.dtpMulai.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpMulai.Location = new System.Drawing.Point(12, 74);
+            this.dtpMulai.Name = "dtpMulai";
+            this.dtpMulai.Size = new System.Drawing.Size(200, 20);
+            this.dtpMulai.TabIndex = 1;
+            // 
+            // dtpSelesai
+            // 
+            this.dtpSelesai.CustomFormat = "dd/MM/yyyy hh:mm:ss";
+            this.dtpSelesai.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpSelesai.Location = new System.Drawing.Point(12, 123);
+            this.dtpSelesai.Name = "dtpSelesai";
+            this.dtpSelesai.Size = new System.Drawing.Size(200, 20);
+            this.dtpSelesai.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nama Kegiatan:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Waktu Mulai Kegiatan:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Waktu Selesai Kegiatan:";
+            // 
+            // btnSimpan
+            // 
+            this.btnSimpan.Location = new System.Drawing.Point(137, 161);
+            this.btnSimpan.Name = "btnSimpan";
+            this.btnSimpan.Size = new System.Drawing.Size(75, 23);
+            this.btnSimpan.TabIndex = 6;
+            this.btnSimpan.Text = "&Simpan";
+            this.btnSimpan.UseVisualStyleBackColor = true;
+            this.btnSimpan.Click += new System.EventHandler(this.btnSimpan_Click);
+            // 
+            // frmDetilKegiatan
+            // 
+            this.AcceptButton = this.btnSimpan;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(224, 196);
+            this.Controls.Add(this.btnSimpan);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dtpSelesai);
+            this.Controls.Add(this.dtpMulai);
+            this.Controls.Add(this.txtNama);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.MaximumSize = new System.Drawing.Size(800, 235);
+            this.MinimumSize = new System.Drawing.Size(240, 235);
+            this.Name = "frmDetilKegiatan";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNama;
+        private System.Windows.Forms.DateTimePicker dtpMulai;
+        private System.Windows.Forms.DateTimePicker dtpSelesai;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSimpan;
+    }
+}
---
+++ WinForms/Forms/frmDetilKegiatan.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WinForms.Class;
+
+namespace WinForms.Forms
+{
+    public partial class frmDetilKegiatan : Form
+    {
+        public Kegiatan kegiatan { get; private set; }
+        
+        public frmDetilKegiatan()
+        {
+            InitializeComponent();
+            this.kegiatan = null;
+            this.Text = "Kegiatan Baru";
+        }
+
+        public frmDetilKegiatan(Kegiatan kegiatan)
+        {
+            InitializeComponent();
+            this.kegiatan = kegiatan;
+            this.Text = "Ubah Kegiatan";
+        }
+
+        private void btnSimpan_Click(object sender, EventArgs e)
+        {
+            if (this.kegiatan == null) // Kegiatan baru
+            {
+                this.kegiatan = new Kegiatan();
+                this.kegiatan.ID = Kegiatan.GetNextID();
+                this.kegiatan.PrepareEmptyKehadiran(Anggota.GetAll());
+            }
+
+            this.kegiatan.Nama = txtNama.Text;
+            this.kegiatan.JamMulai = dtpMulai.Value;
+            this.kegiatan.JamSelesai = dtpSelesai.Value;
+            this.kegiatan.Save();
+
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+    }
+}
