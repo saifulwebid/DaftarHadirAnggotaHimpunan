9807f610510fb2cfc5edc1f024609df7d00192d0
Muhammad Saiful Islam <muhammad@saiful.web.id>
2015-06-26 14:37:38 +0700

    Mengganti "Telat" menjadi "Terlambat"

--- WinForms/Class/Enum.cs
+++ WinForms/Class/Enum.cs
@@ -3,5 +3,5 @@
 namespace WinForms.Class
 {
     [Flags]
-    public enum JenisKehadiran { Alpa, Hadir, Telat, Sakit, Izin }
+    public enum JenisKehadiran { Alpa, Hadir, Terlambat, Sakit, Izin }
 }
