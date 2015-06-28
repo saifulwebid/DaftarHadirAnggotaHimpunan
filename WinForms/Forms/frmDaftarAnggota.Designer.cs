ea3c21df99bcbcd9c24374665a9c95f0fb326b55
Fadhlan Ridhwanallah <fridhwanallah@gmail.com>
2015-06-28 21:43:09 +0700

    Menambah fitur import dari excel versi 1

--- WinForms/Forms/frmDaftarAnggota.Designer.cs
+++ WinForms/Forms/frmDaftarAnggota.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace WinForms.Forms
+﻿amespace WinForms.Forms
 {
     partial class frmDaftarAnggota
     {
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             this.dgvAnggota = new System.Windows.Forms.DataGridView();
+            this.btnImport = new System.Windows.Forms.Button();
+            this.tbFile = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvAnggota)).BeginInit();
             this.SuspendLayout();
             // 
@@ -38,27 +41,60 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dgvAnggota.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvAnggota.Location = new System.Drawing.Point(12, 12);
+            this.dgvAnggota.Location = new System.Drawing.Point(12, 54);
             this.dgvAnggota.Name = "dgvAnggota";
-            this.dgvAnggota.Size = new System.Drawing.Size(566, 433);
+            this.dgvAnggota.Size = new System.Drawing.Size(566, 391);
             this.dgvAnggota.TabIndex = 0;
             this.dgvAnggota.RowValidating += new System.Windows.Forms.DataGridViewCellCancelEventHandler(this.dgvAnggota_RowValidating);
+            // 
+            // btnImport
+            // 
+            this.btnImport.Location = new System.Drawing.Point(483, 12);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(64, 29);
+            this.btnImport.TabIndex = 1;
+            this.btnImport.Text = "Import";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // tbFile
+            // 
+            this.tbFile.Location = new System.Drawing.Point(358, 17);
+            this.tbFile.Name = "tbFile";
+            this.tbFile.Size = new System.Drawing.Size(108, 20);
+            this.tbFile.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(288, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nama File";
             // 
             // frmDaftarAnggota
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(590, 457);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbFile);
+            this.Controls.Add(this.btnImport);
             this.Controls.Add(this.dgvAnggota);
             this.Name = "frmDaftarAnggota";
             this.Text = "Daftar Anggota";
             ((System.ComponentModel.ISupportInitialize)(this.dgvAnggota)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dgvAnggota;
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.TextBox tbFile;
+        private System.Windows.Forms.Label label1;
     }
 }
--- WinForms/Forms/frmDaftarAnggota.cs
+++ WinForms/Forms/frmDaftarAnggota.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OfficeOpenXml;
+using OfficeOpenXml.Style;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,6 +10,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using WinForms.Class;
+using System.IO;
 
 namespace WinForms.Forms
 {
@@ -34,6 +37,52 @@ namespace WinForms.Forms
             if (dgvAnggota.IsCurrentRowDirty)
             {
                 ((Anggota)dgvAnggota.Rows[e.RowIndex].DataBoundItem).Save();
+            }
+        }
+
+        Anggota anggota = new Anggota();
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            if (tbFile.Text == null)
+            {
+                MessageBox.Show("Masukkan alamat file!!!");
+                /*Jika tbFIle belum terisi*/
+            }
+            else{
+                FileInfo newFile = new FileInfo(tbFile.Text);
+                if (newFile != null)
+                {
+                    using (ExcelPackage package = new ExcelPackage(newFile))
+                    {
+                        ExcelWorkbook workBook = package.Workbook;
+                        if (workBook != null)
+                        {
+                            ExcelWorksheet worksheet = workBook.Worksheets[1];
+
+                            for (int i = 5; i <= worksheet.Dimension.End.Row; i++)
+                            {
+                                anggota.NomorAnggota = worksheet.Cells[i, 3].Text;
+                                anggota.NomorMahasiswa = worksheet.Cells[i, 4].Text;
+                                anggota.Nama = worksheet.Cells[i, 5].Text;
+                                anggota.NamaBagus = worksheet.Cells[i, 6].Text;
+                                anggota.Kelas = worksheet.Cells[i, 7].Text;
+                                anggota.Departemen = worksheet.Cells[i, 8].Text;
+                                anggota.NomorHandphone = worksheet.Cells[i, 9].Text;
+                            }
+                                try
+                                {
+                                    package.Save();
+                                }
+                                catch (Exception ex)
+                                {
+                                    MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                                }
+
+                            MessageBox.Show("Data anggota berhasil di-import.",
+                                "Berhasil", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                    }
+                }
             }
         }
     }
